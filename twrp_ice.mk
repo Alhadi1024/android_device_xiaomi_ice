c670a0d1499c8203be0f19cfff7e410029681890
Alhadi1024 <117118314+Alhadi1024@users.noreply.github.com>
2023-08-12 16:10:40 +0600

    Update and rename omni_ice.mk to twrp_ice.mk

--- twrp_ice.mk
+++ twrp_ice.mk
@@ -9,13 +9,13 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from ice device
 $(call inherit-product, device/xiaomi/ice/device.mk)
 
 PRODUCT_DEVICE := ice
-PRODUCT_NAME := omni_ice
+PRODUCT_NAME := twrp_ice
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := ice
 PRODUCT_MANUFACTURER := xiaomi
